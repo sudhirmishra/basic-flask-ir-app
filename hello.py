ceee71d7aa5adf9e21656fbb42bbad49d45460c0
Sudhir Mishra <sudhirxps@gmail.com>
2016-11-25 08:33:38 +0530

    Tagging added

--- hello.py
+++ hello.py
@@ -4,7 +4,13 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route('/')
+def index():
+
+	return render_template('index.html')
+
+@app.route('/rating',methods=['GET','POST'])
 def hello_world():
 	movies = {}
 	if request.method == "POST":
@@ -15,7 +21,15 @@ def hello_world():
 				continue
 			movies[item] = request.form[item]
 
-		print(movies)
+			"""
+			Structure
+			
+			movies = { "movie-name-1":"rating" }
+			"""
+		for key, value in movies.items():
+			print("Movie name: "+key+" rated "+value)
+
+		# To see the above print statements, look at linux bash/shell/command prompt
 
 		return message
 
@@ -25,6 +39,35 @@ def hello_world():
 
 		return render_template('hello.html', movies=movies)
 	
+@app.route('/tagging',methods=['GET','POST'])
+def tagging():
 
+	movies = {}
+	if request.method == "POST":
+		message = "Processed Data!"
+		
+		for item in request.form:
+			if item == "singlebutton":
+				continue
+			movies[item] = request.form[item].split(',')
 
+		"""
+		Structure
+		
+		movies = { "movie-name-1":["tag-1","tag-2"] }
+		"""
 
+		for key,value in movies.items():
+			print("Movie name "+key+" tags "+ "|".join(value))
+
+		# To see the above print statements, look at linux bash/shell/command prompt
+
+		print(movies)
+
+		return message
+
+	else:
+
+		movies = [["A B"],["B"],["C"],["D"],["E"]]
+		return render_template('tagging.html', movies=movies)
+	
